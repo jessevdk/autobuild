fe2dbef250e166fb152b1ad634eab24d2cceda6e
Jesse van den Kieboom <jessevdk@gnome.org>
2012-11-29 10:55:46 +0100

    Added lookup group id

--- group.go
+++ group.go
@@ -5,6 +5,7 @@ package main
 #include <grp.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 static int
 find_group_member (char const *group, char const *member)
@@ -31,11 +32,27 @@ find_group_member (char const *group, char const *member)
 
 	return 0;
 }
+
+static int
+lookup_group (char const *group)
+{
+	struct group *g;
+
+	g = getgrnam (group);
+
+	if (!g)
+	{
+		return -1;
+	}
+
+	return (int)g->gr_gid;
+}
 */
 import "C"
 
 import (
 	"unsafe"
+	"fmt"
 )
 
 func userIsMemberOfGroup(user string, group string) bool {
@@ -48,4 +65,16 @@ func userIsMemberOfGroup(user string, group string) bool {
 	C.free(unsafe.Pointer(u))
 
 	return val == 1
+}
+
+func lookupGroupId(group string) (uint32, error) {
+	g := C.CString(group)
+	val := C.lookup_group(g)
+	C.free(unsafe.Pointer(g))
+
+	if val == -1 {
+		return 0, fmt.Errorf("Group `%s' does not exist", group)
+	}
+
+	return uint32(val), nil
 }
