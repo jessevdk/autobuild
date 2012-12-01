fed53f75af7d4305cdfcd3f35e925302818b13a6
Jesse van den Kieboom <jessevdk@gnome.org>
2012-12-01 16:26:47 +0100

    Added autobuild description

--- man.go
+++ man.go
@@ -8,5 +8,5 @@ func main() {
 	parser.ApplicationName = "autobuild"
 
 	parser.WriteManPage(os.Stdout,
-	                    "Autobuild description")
+	                    "autobuild is an easy to use debian package build system. It's main purpose is to simplify the process of maintaing various pieces of the debian package build system (mainly pbuilder and reprepro). autobuild provides a single entry point to managing the build process.")
 }
