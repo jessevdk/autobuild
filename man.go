a8634dda449acb2c9d63c5ac9f0154add504b522
Jesse van den Kieboom <jessevdk@gnome.org>
2012-12-01 16:36:03 +0100

    Higlight programs in man description

--- man.go
+++ man.go
@@ -8,5 +8,5 @@ func main() {
 	parser.ApplicationName = "autobuild"
 
 	parser.WriteManPage(os.Stdout,
-	                    "autobuild is an easy to use debian package build system. It's main purpose is to simplify the process of maintaing various pieces of the debian package build system (mainly pbuilder and reprepro). autobuild provides a single entry point to managing the build process.")
+	                    "`autobuild' is an easy to use debian package build system. It's main purpose is to simplify the process of maintaing various pieces of the debian package build system (mainly `pbuilder' and `reprepro'). `autobuild' provides a single entry point to managing the build process.")
 }
